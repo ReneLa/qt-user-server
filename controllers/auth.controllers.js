16eb502e5942cbe2f7ea5cda065901e183191309
Rene La <renela79@gmail.com>
2023-11-28 12:42:11 +0200

    feat: password hashing on signup

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,13 +1,17 @@
-import { sign, verify } from "jsonwebtoken";
-import { db } from "../lib/db";
+const { sign, verify } = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-export const newToken = (user) => {
+const { db } = require("../lib/db");
+
+const saltRounds = 10;
+
+const newToken = (user) => {
   return sign({ id: user.id }, process.env.SECRET, {
     expiresIn: "365d"
   });
 };
 
-export const verifyToken = (token) =>
+const verifyToken = (token) =>
   new Promise((resolve, reject) => {
     verify(token, process.env.SECRET, (err, payload) => {
       if (err) return reject(err);
@@ -15,62 +19,78 @@ export const verifyToken = (token) =>
     });
   });
 
-export const signup = async (req, res) => {
-  const { email, password, confirm_password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).send({ message: "need email and password" });
-  }
-
-  if (password !== confirm_password) {
-    return res.status(400).send({ message: "Password don't match" });
-  }
+const signup = async (req, res) => {
   try {
-    const user = await db.user.create(req.body);
-    const token = newToken(user);
-    return res.status(201).send({ user: { email, username }, token });
+    const { email, password, confirm_password } = req.body;
+    if (!email || !password) {
+      return res.status(400).send({ message: "need email and password" });
+    }
+
+    if (password !== confirm_password) {
+      return res.status(400).send({ message: "Password don't match" });
+    }
+    let hashedPassword;
+    await bcrypt.hash(password, saltRounds).then(function (hash) {
+      hashedPassword = hash;
+    });
+
+    const newUser = await db.user.create({
+      data: {
+        email,
+        password: hashedPassword
+      },
+      select: {
+        email: true,
+        first_name: true,
+        last_name: true,
+        title: true,
+        caption: true
+      }
+    });
+    const token = newToken(newUser);
+    return res.status(201).send({ user: { email }, token });
   } catch (e) {
     console.log(e);
-    return res.status(500).end();
+    return res.status(500).send({ message: "Server Error" }).end();
   }
 };
 
-export const signin = async (req, res) => {
+const signin = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
-    return res.status(400).send({ message: "need email and password" });
+    return res.status(400).send({ message: "Need email and password" });
   }
 
-  const invalid = { message: "Invalid email and password combination" };
-
   try {
-    const user = await db.user
-      .findOne({ email })
-      .select("username email password")
-      .exec();
+    const existingUser = await db.user.findUnique({
+      where: {
+        email
+      }
+    });
 
-    if (!user) {
-      return res.status(401).send(invalid);
+    if (!existingUser) {
+      return res.status(401).send({ message: "User does not exist" });
     }
 
-    const match = await user.checkPassword(password);
+    const match = await bcrypt.compare(password, existingUser.password);
 
     if (!match) {
-      return res.status(401).send(invalid);
+      return res.status(401).send({ message: "Wrong password" });
     }
 
-    const token = newToken(user);
+    const token = newToken(existingUser);
 
-    return res
-      .status(201)
-      .send({ user: { username: user.username, email: user.email }, token });
+    return res.status(200).send({
+      user: { username: existingUser.username, email: existingUser.email },
+      token
+    });
   } catch (e) {
     console.error(e);
-    res.status(500).end();
+    res.status(500).send({ message: "Server Error" }).end();
   }
 };
 
-export const protect = async (req, res, next) => {
+const protect = async (req, res, next) => {
   const bearer = req.headers.authorization;
 
   if (!bearer || !bearer.startsWith("Bearer ")) {
@@ -85,16 +105,25 @@ export const protect = async (req, res, next) => {
     return res.status(401).end();
   }
 
-  const user = await db.user
-    .findById(payload.id)
-    .select("-password")
-    .lean()
-    .exec();
+  const user = await db.user.findById({
+    where: {
+      id: payload.id
+    },
+    select: {
+      email: true,
+      first_name: true,
+      last_name: true,
+      title: true,
+      caption: true
+    }
+  });
 
   if (!user) {
-    return res.status(401).end();
+    return res.status(401).send({ message: "Unauthorized user" }).end();
   }
 
   req.user = user;
   next();
 };
+
+module.exports = { signin, signup, protect };
